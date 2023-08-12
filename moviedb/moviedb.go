00d690a9603fb4860a19edd55f22499e75f09349
Shrinibas Mahanta <shrinibasmahanta2004@gmail.com>
2023-08-12 13:26:37 +0530

    This replaces deprecated ioutil with io

--- moviedb/moviedb.go
+++ moviedb/moviedb.go
@@ -3,7 +3,7 @@ package moviedb
 import (
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 	"net/url"
 )
@@ -38,7 +38,7 @@ func (c *Client) FetchMovie(query string) (*Movies, error) {
 		return nil, err
 	}
 	defer resp.Body.Close()
-	body, err := ioutil.ReadAll(resp.Body)
+	body, err := io.ReadAll(resp.Body)
 
 	if err != nil {
 		return nil, err
